d5e2ada17451b46e6ace7762dcf01597323a559b
tomsmith01 <thomas.smith2110@gmail.com>
2022-04-14 21:27:09 +0100

    init

--- Main.py
+++ Main.py
@@ -5,6 +5,6 @@ from spotipy.oauth2 import SpotifyOAuth
 window = Tk()
 window.title("Spotify Stats Viewer")
 
-window.configure(background='#3A3B3C', width=800, height=500)
+window.configure(background='#3A3B3C', width=900, height=500)
 
 window.mainloop()
