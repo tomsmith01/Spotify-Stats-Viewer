f40cd81b77fa94b98c9d0d4b93000c811a626617
tomsmith01 <thomas.smith2110@gmail.com>
2022-04-14 21:22:20 +0100

    First information getter class creating, simple TkInter window created

---
+++ Main.py
@@ -0,0 +1,10 @@
+from tkinter import Tk
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+window = Tk()
+window.title("Spotify Stats Viewer")
+
+window.configure(background='#3A3B3C', width=800, height=500)
+
+window.mainloop()
